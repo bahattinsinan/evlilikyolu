16bd3e9a474b856e9a6dc254f05faa15f4ed5a7c
bahattinsinan <bahattinsinan1@gmail.com>
2025-07-20 15:26:55 +0000

    firebaseConfig.js oluşturuldu

---
+++ public/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXDPSHzYwDKJMKflRPaC-egd-r6dLzL6U",
+  authDomain: "evlilikyolutr.firebaseapp.com",
+  projectId: "evlilikyolutr",
+  storageBucket: "evlilikyolutr.firebasestorage.app",
+  messagingSenderId: "299542496680",
+  appId: "1:299542496680:web:79967f42f243b64c4dc99d",
+  measurementId: "G-RVZYMG4T3M"
+};
